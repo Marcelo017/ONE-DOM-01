f9be6e339b257828a09b9f4dd024a47aa226c808
Marcelo <marcelogrando017@gmail.com>
2022-04-21 05:12:29 -0300

    clase03 finish

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 const btnAdd = document.querySelector('[data-form-btn]');
 
+
 const createTask = (e) => {
   e.preventDefault()
   console.log('hiciste click')
   const input = document.querySelector("[data-form-input]");
-  console.log(input.value)
+  const value = input.value
+  const list = document.querySelector('[data-list]');
+  const task = document.createElement('li');
+  task.classList.add('card')
+  input.value = '';
+  const content = `<div>
+  <i class="fas fa-check-square completedIcon icon"></i>
+  <span class="task">${value}</span>
+  <i class="fas fa-check-squareX"></i>
+</div>`
+  task.innerHTML = content;
+  list.appendChild(task)
 }
 
 

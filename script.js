7a14399053a8b73a52a225d0545d589f3b65fd3d
Marcelo <marcelogrando017@gmail.com>
2022-04-21 04:53:52 -0300

    clase02 finish

--- script.js
+++ script.js
@@ -1,30 +1,11 @@
-import checkComplete from './components/checkComplete.js';
-import deleteIcon from './components/deleteIcon.js';
+const btnAdd = document.querySelector('[data-form-btn]');
 
-const btn = document.querySelector('[data-form-btn]');
+const createTask = (e) => {
+  e.preventDefault()
+  console.log('hiciste click')
+  const input = document.querySelector("[data-form-input]");
+  console.log(input.value)
+}
 
-const createTask = (evento) => {
-  evento.preventDefault();
-  const input = document.querySelector('[data-form-input]');
-  const value = input.value;
-  const list = document.querySelector('[data-list]');
-  const task = document.createElement('li');
-  task.classList.add('card');
-  input.value = '';
-  //backticks
-  const taskContent = document.createElement('div');
 
-  const titleTask = document.createElement('span');
-  titleTask.classList.add('task');
-  titleTask.innerText = value;
-  taskContent.appendChild(checkComplete());
-  taskContent.appendChild(titleTask);
-  // task.innerHTML = content;
-
-  task.appendChild(taskContent);
-  task.appendChild(deleteIcon());
-  list.appendChild(task);
-};
-
-//Arrow functions o funciones anonimas
-btn.addEventListener('click', createTask);
+btnAdd.addEventListener('click', createTask)
